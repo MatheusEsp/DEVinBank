a4b941529b469c95f6f95e736596dd614379814e
matheus <mathespindola12@gmail.com>
2022-10-30 21:11:44 -0300

    FInal

--- assets/scripts/bank.js
+++ assets/scripts/bank.js
@@ -1,15 +1,17 @@
-let contasClientes = []
+let contasClientes = [];
 
 const validarSenhasIguais = (evento) => {
 
   if (evento.target.senha.value == evento.target.confirmacao.value) {
-    return true
+    return true;
   }
-  return false
-}
+
+  return false;
+};
 
 const cadastrarConta = (evento) => {
-  evento.preventDefault()
+  evento.preventDefault();
+
   if (validarSenhasIguais(evento)) {
     const conta = {
       nome: evento.target.nome.value,
@@ -19,73 +21,122 @@ const cadastrarConta = (evento) => {
       conta: Math.floor(1000 + Math.random() * 90000),
       saldo: 0,
     };
-    contasClientes.push(conta)
-    alert(`Conta criada! Número: ${conta.conta}`)
+
+    contasClientes.push(conta);
+    alert(`Conta criada com sucesso! Número: ${conta.conta}`);
   } else {
-    alert('Senhas não são iguais')
+    alert('Senhas não conferem');
   }
-}
+};
 
-const form = document.getElementById('form')
-form.addEventListener('submit', cadastrarConta)
+const form = document.getElementById('form');
+form.addEventListener('submit', cadastrarConta);
 
-
+// Operações
 const trocarOperacao = (evento) => {
-  const valor = document.getElementById('valor')
-  valor.disabled = evento.target.value == 'SALDO'
+  const valor = document.getElementById('valor');
+
+  valor.disabled = evento.target.value == 'SALDO';
 };
 
 const obterConta = (conta) => {
-    const contaCliente = contasClientes.find((c) => c.conta === conta);
-  
-    return contaCliente;
-  };
+  const contaCliente = contasClientes.find((c) => c.conta == conta);
 
-
-const saque = () => {}
-
-const deposito = () => {}
-
-const consultaSaldo = (conta) => {
-    const contaCliente = obterConta(conta);
-
-    alert(`Saldo atual: ${contaCliente.saldo}`);
+  return contaCliente;
 };
 
-const validaConta = (conta, senha) => {
-    const contaCliente = obterConta(conta)
-  return contaCliente && contaCliente.senha == senha ? true : false
-}
+const sacar = (conta, valor) => {
+  if (validarValor(valor)) {
+    if (validarSaldo(conta, valor)) {
+      let saldoAtual;
+      const contasAtualizadas = contasClientes.map((c) => {
+        if (c.conta === conta) {
+          saldoAtual = c.saldo - valor;
+          return { ...c, saldo: saldoAtual };
+        }
+        return c;
+      });
+
+      contasClientes = contasAtualizadas;
+
+      alert(`Saque efetuado com sucesso! Saldo atual: ${saldoAtual}`);
+    } else {
+      alert('Saldo insuficiente');
+    }
+  } else {
+    alert('Valor inválido');
+  }
+};
+
+const depositar = (conta, valor) => {
+  if (validarValor(valor)) {
+
+
+    const contaCliente = { ...obterConta(conta) };
+    contaCliente.saldo += valor;
+
+    const contasAtualizadas = contasClientes.filter((c) => c.conta !== conta);
+    contasAtualizadas.push(contaCliente);
+    contasClientes = contasAtualizadas;
+
+    alert(`Deposito efetuado com sucesso! Saldo atual: ${contaCliente.saldo}`);
+  } else {
+    alert('Valor inválido');
+  }
+};
+
+const consultarSaldo = (conta) => {
+  const contaCliente = obterConta(conta);
+
+  alert(`Saldo atual: ${contaCliente.saldo}`);
+};
+
+const validarConta = (conta, senha) => {
+  const contaCliente = obterConta(conta);
+
+  return contaCliente && contaCliente.senha === senha ? true : false;
+};
+
+const validarValor = (valor) => {
+  return !isNaN(valor) && valor > 0;
+};
+
+const validarSaldo = (conta, valor) => {
+  const contaCliente = obterConta(conta);
+
+  return contaCliente.saldo >= valor;
+};
 
 const efetuarOperacao = (evento) => {
-  evento.preventDefault()
+  evento.preventDefault();
+
   const conta = parseInt(evento.target.conta.value);
   const senha = evento.target.senha.value;
+  const valor = parseInt(evento.target.valor.value);
 
-  const contaValida = validaConta(conta, senha);
+  const contaValida = validarConta(conta, senha);
 
   if (contaValida) {
     switch (evento.target.operacao.value) {
       case 'SAQUE':
-        saque()
-        break
+        sacar(conta, valor);
+        break;
       case 'DEPOSITO':
-        deposito()
-        break
+        depositar(conta, valor);
+        break;
       case 'SALDO':
-        consultaSaldo()
-        consultaSaldo(conta)
-        break
+        consultarSaldo(conta);
+        break;
       default:
-        alert('Operação inválida')
+        alert('Operação inválida');
     }
   } else {
-    alert('Conta ou senha inválida')
+    alert('Conta ou senha inválida');
   }
-}
+};
 
-const operacao = document.getElementById('operacao')
-operacao.addEventListener('change', trocarOperacao)
+const operacao = document.getElementById('operacao');
+operacao.addEventListener('change', trocarOperacao);
 
-const formAcoes = document.getElementById('form-acoes')
-formAcoes.addEventListener('submit', efetuarOperacao)
+const formAcoes = document.getElementById('form-acoes');
+formAcoes.addEventListener('submit', efetuarOperacao);
